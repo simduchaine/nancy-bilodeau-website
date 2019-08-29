56b5f95014c7e0fd07a204f922da5b177e9fa2f7
Simon Duchaine <simon.duchaine@hotmail.com>
2019-08-29 14:27:27 -0400

    fix form name

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -19,21 +19,21 @@
               <div class="field">
                 <label class="label">Nom</label>
                 <div class="control">
-                  <input class="input" type="text" />
+                  <input class="input" type="text" name="nom" />
                 </div>
               </div>
 
               <div class="field">
                 <label class="label">Email</label>
                 <div class="control">
-                  <input class="input" type="email" />
+                  <input class="input" type="email" name="email" />
                 </div>
               </div>
 
               <div class="field">
                 <label class="label">Message</label>
                 <div class="control">
-                  <textarea class="textarea"></textarea>
+                  <textarea class="textarea" name="message"></textarea>
                 </div>
               </div>
 
