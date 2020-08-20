b4980e4609521b45a7f0681798cb68b26333a8ed
Simon Duchaine <simon.duchaine@hotmail.com>
2020-08-20 09:45:08 -0400

    test deploy without slide image

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,10 +9,10 @@
         :arrows="true"
       >
         <div v-for="slide in $page.slides.edges" :key="slide.node.id">
-          <g-image
+          <!-- <g-image
             class="hero-background is-transparent"
             :src="slide.node.image"
-          />
+          /> -->
           <div class="hero-body">
             <div class="container">
               <h1 class="title is-size-2">{{ slide.node.title }}</h1>
